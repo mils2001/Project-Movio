7fd5b4cd572d91e5e5996fde2b81bd24b3e6abf5
chris <Mileschris466@gmail.com>
2024-11-06 23:33:15 +0300

    movio

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './Component/Navbar/Navbar'
+import Nap from './Component/Nap'
+function App() {
+  return (
+    <div className="App">
+     <Navbar/>
+     <Nap/>
+     <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        Learn React
+      </a>
+    </div>
+  
+  );
+}
+
+export default App;
