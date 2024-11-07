3f95d32d4f36eb1cf2d618039b3306dc5ddd7996
chris <Mileschris466@gmail.com>
2024-11-07 13:03:08 +0300

    navbar added

--- src/Component/Navbar/Navbar.jsx
+++ src/Component/Navbar/Navbar.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+// Navbar.js
+import React from 'react';
+import './Navbar.css'; // Import CSS for styling
 
-function  Navbar() {
+const Navbar = () => {
   return (
-     
-      <nav>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+    <nav className="navbar">
+      <div className="logo" >MOVIO</div>
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
-      </nav>
-    
-  )
-}
+      <div className="nav-toggle">☰</div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
