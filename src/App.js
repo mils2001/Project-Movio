91f32e354f48d3d2a9aa49746a2e592e91f37d51
chris <Mileschris466@gmail.com>
2024-11-23 01:02:52 +0300

    fetch

--- src/App.js
+++ src/App.js
@@ -1,63 +1,8 @@
-import React, { useEffect, useState }  from 'react'                                                                                                                             
+import React  from 'react'                                                                                                                             
 import './App.css';
 import Navbar  from './component/Navbar/Navbar.jsx'
 import Footer from './component/Footer.jsx'
-
-const API_URL = 'http://localhost:3005/films';
-
-const movieApp = () => {
-  const [movieList, setMovieList] = useState([]);
-  const [Newmovie, setNewMovie] = useState({
-    title: '',
-    description: '',
-    poster: '',
-    capacity: '',
-    runtime: '',
-    id:'null',
-  });
-  Const [editMovie, setEditMovie] = useState ([]);
-
- useEffect(() => {
-  const FetchMovie = async () => {
-    try{
-      const response = await fetch(API_URL);
-      const data = await response.json();
-      setMovieList(data, movieList);
-    } catch(error) {
-      console.error('Error:', error);
-    }
-  };
- 
-  setMovieList();
-
- }, []);
- const HandleMovieList = async(event) =>{
-    event.preventDefault();
-    try{
-      const response = await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(Newmovie),
-      });
-      const data = await response.json();
-      setMovieList([...movieList, data]);
-      setNewMovie({
-        title: '',
-        description: '',
-        poster: '',
-        capacity: '',
-        runtime: '',
-        id:'null',
-      });
-    } catch(error) {
-      console.error('Error:', error);
-    }
- 
- }
-}
-
+import MovieApp from './index.js';
 
 
 
@@ -66,10 +11,10 @@ function App() {
     <div className="App">
      <Navbar/>
      <Footer/>
-     
+     <MovieApp/>
      
      <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
+    
       </a>
     </div>
   
--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const API_URL = 'http://localhost:3005/films';
+
+function MovieApp () {
+  const [movieList, setMovieList] = useState([]);
+  const [Newmovie, setNewMovie] = useState({
+    title: '',
+    description: '',
+    poster: '',
+    capacity: '',
+    runtime: '',
+    id:'null',
+  });
+  Const[editMovie, setEditMovie] = useState ([]);
+
+ useEffect(() => {
+  function FetchMovie() {
+     fetch(API_URL)
+      const response => await fetch(API_URL);
+      const data => await response.json();
+      setMovieList(data, movieList);
+    } catch(error) {
+      console.error('Error:', error);
+    
+  };
+ 
+  setMovieList();
+
+ }, []);
+ const HandleMovieList = async(event) =>{
+    event.preventDefault();
+    try{
+      const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(Newmovie),
+      });
+      const data = await response.json();
+      setMovieList([...movieList, data]);
+      setNewMovie({
+        title: '',
+        description: '',
+        poster: '',
+        capacity: '',
+        runtime: '',
+        id:'null',
+      });
+    } catch(error) {
+      console.error('Error:', error);
+    }
+ 
+ }
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +70,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default MovieApp
