b4061204a26959f0a332a02406b39a9d9eebd49c
chris <Mileschris466@gmail.com>
2024-11-30 19:47:02 +0300

    CSS CONTACT

--- src/component/Navbar/pages/About.js
+++ src/component/Navbar/pages/About.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='About-App'>
+    <div className='About'>
         <h1>About Molvotv Entertainment</h1>
         <p>We are a renowned movie and entertainment company specializing in providing high-quality content.</p>
         <p>Our team of experts is committed to providing you with the best possible movie experience.</p>
