b080a66e09b5269c2a6b02b2fddb20b9d67a084b
chris <Mileschris466@gmail.com>
2024-12-02 10:50:17 +0300

    debugging

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./component/Navbar/Navbar";
+import MovieApp from "./MovieApp";
 import Home from "./component/Navbar/pages/Home";
 import About from "./component/Navbar/pages/About";
 import Contact from "./component/Navbar/pages/Contact";
-import MovieApp from "./MovieApp";
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<MovieApp />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/movies" element={<MovieApp />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
 
 
--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,35 +1,29 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'; // Assuming Navbar.css exists
-// Make sure this file exists
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: 'Welcome',
+      message: "Welcome",
       isSignUpPopupVisible: false,
       isSignInPopupVisible: false,
-      signUpDetails: { name: '', email: '', password: '' },
-      signInDetails: { email: '', password: '' },
+      signUpDetails: { name: "", email: "", password: "" },
+      signInDetails: { email: "", password: "" },
     };
   }
 
-  // Toggle Sign-Up Popup
-  toggleSignUpPopup = () => {
+  toggleSignUpPopup = () =>
     this.setState((prevState) => ({
       isSignUpPopupVisible: !prevState.isSignUpPopupVisible,
     }));
-  };
 
-  // Toggle Sign-In Popup
-  toggleSignInPopup = () => {
+  toggleSignInPopup = () =>
     this.setState((prevState) => ({
       isSignInPopupVisible: !prevState.isSignInPopupVisible,
     }));
-  };
 
-  // Handle Sign-Up Input Changes
   handleSignUpChange = (event) => {
     const { name, value } = event.target;
     this.setState((prevState) => ({
@@ -37,7 +31,6 @@ class Navbar extends Component {
     }));
   };
 
-  // Handle Sign-In Input Changes
   handleSignInChange = (event) => {
     const { name, value } = event.target;
     this.setState((prevState) => ({
@@ -45,155 +38,110 @@ class Navbar extends Component {
     }));
   };
 
-  // Handle Sign-Up Form Submission
   handleSignUpSubmit = (event) => {
     event.preventDefault();
     const { signUpDetails } = this.state;
-    console.log('Sign-Up Details:', signUpDetails);
-    this.setState({
-      message: 'You have signed up!',
-      isSignUpPopupVisible: false,
-      signUpDetails: { name: '', email: '', password: '' },
-    });
+    alert("Sign-Up Successful!");
+    console.log("Sign-Up Details:", signUpDetails);
+    this.toggleSignUpPopup();
   };
 
-  // Handle Sign-In Form Submission
   handleSignInSubmit = (event) => {
     event.preventDefault();
     const { signInDetails } = this.state;
-    console.log('Sign-In Details:', signInDetails);
-    this.setState({
-      message: 'Welcome Back!',
-      isSignInPopupVisible: false,
-      signInDetails: { email: '', password: '' },
-    });
+    alert("Sign-In Successful!");
+    console.log("Sign-In Details:", signInDetails);
+    this.toggleSignInPopup();
   };
 
   render() {
-    const {
-      message,
-      isSignUpPopupVisible,
-      isSignInPopupVisible,
-      signUpDetails,
-      signInDetails,
-    } = this.state;
+    const { message, isSignUpPopupVisible, isSignInPopupVisible, signUpDetails, signInDetails } = this.state;
 
     return (
-      
-        <div>
-          {/* Navbar */}
-          <nav className="navbar">
-            <div className="logo">MOVIO</div>
-            <ul className="nav-links">
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li>
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
-            <div className="message">{message}</div>
-            <button className="gold-button" onClick={this.toggleSignUpPopup}>
-              Sign up
-            </button>
-            <button className="gold-button" onClick={this.toggleSignInPopup}>
-              Sign in
-            </button>
-          </nav>
+      <div>
+        <nav className="navbar">
+          <div className="logo">MOVIO</div>
+          <ul className="nav-links">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+          <div className="message">{message}</div>
+          <button className="gold-button" onClick={this.toggleSignUpPopup}>
+            Sign Up
+          </button>
+          <button className="gold-button" onClick={this.toggleSignInPopup}>
+            Sign In
+          </button>
+        </nav>
 
-        
-
-          {/* Sign-Up Popup */}
-          {isSignUpPopupVisible && (
-            <div className="popup-overlay">
-              <div className="popup">
-                <h3>Sign Up</h3>
-                <form onSubmit={this.handleSignUpSubmit}>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    value={signUpDetails.name}
-                    onChange={this.handleSignUpChange}
-                    required
-                  />
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={signUpDetails.email}
-                    onChange={this.handleSignUpChange}
-                    required
-                  />
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={signUpDetails.password}
-                    onChange={this.handleSignUpChange}
-                    required
-                  />
-                  <button type="submit" className="subs">
-                    Submit
-                  </button>
-                  <button
-                    type="button"
-                    onClick={this.toggleSignUpPopup}
-                    className="close"
-                  >
-                    Close
-                  </button>
-                </form>
-              </div>
+        {isSignUpPopupVisible && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <h3>Sign Up</h3>
+              <form onSubmit={this.handleSignUpSubmit}>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="Name"
+                  value={signUpDetails.name}
+                  onChange={this.handleSignUpChange}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={signUpDetails.email}
+                  onChange={this.handleSignUpChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={signUpDetails.password}
+                  onChange={this.handleSignUpChange}
+                />
+                <button type="submit">Submit</button>
+                <button onClick={this.toggleSignUpPopup}>Close</button>
+              </form>
             </div>
-          )}
+          </div>
+        )}
 
-          {/* Sign-In Popup */}
-          {isSignInPopupVisible && (
-            <div className="popup-overlay">
-              <div className="popup">
-                <h3>Sign In</h3>
-                <form onSubmit={this.handleSignInSubmit}>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    value={signInDetails.email}
-                    onChange={this.handleSignInChange}
-                    required
-                  />
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={signInDetails.password}
-                    onChange={this.handleSignInChange}
-                    required
-                  />
-                  <button type="submit" className="subs">
-                    Submit
-                  </button>
-                  <button
-                    type="button"
-                    onClick={this.toggleSignInPopup}
-                    className="close"
-                  >
-                    Close
-                  </button>
-                </form>
-              </div>
+        {isSignInPopupVisible && (
+          <div className="popup-overlay">
+            <div className="popup">
+              <h3>Sign In</h3>
+              <form onSubmit={this.handleSignInSubmit}>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  value={signInDetails.email}
+                  onChange={this.handleSignInChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={signInDetails.password}
+                  onChange={this.handleSignInChange}
+                />
+                <button type="submit">Submit</button>
+                <button onClick={this.toggleSignInPopup}>Close</button>
+              </form>
             </div>
-          )}
-        </div>
-    
+          </div>
+        )}
+      </div>
     );
   }
 }
 
 export default Navbar;
-
-
-
